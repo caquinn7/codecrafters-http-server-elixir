bf03f7d374994e0e1a264d2ab9b59dd50a30139e
Christopher Quinn <caquinn7@gmail.com>
2025-02-01 15:53:16 -0600

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -2,38 +2,60 @@ defmodule Server do
   use Application
 
   def start(_type, _args) do
-    Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
+    children = [
+      {Task, fn -> Server.listen() end}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 
   def listen() do
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    IO.puts("Logs from your program will appear here!")
-
     # Since the tester restarts your program quite often, setting SO_REUSEADDR
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
     IO.puts("Listening...")
-
     {:ok, client} = :gen_tcp.accept(socket)
-    IO.puts("Accepted connection...")
-
     {:ok, request} = :gen_tcp.recv(client, 0)
     IO.puts("Received request:\n#{request}")
 
-    req_line =
-      String.split(request, "\r\n")
-      |> List.first
-      |> String.split(" ")
-
-    [_, req_target | _] = req_line
-
-    resp = case req_target do
-      "/" -> "HTTP/1.1 200 OK\r\n\r\n"
-      _ -> "HTTP/1.1 404 Not Found\r\n\r\n"
-    end
-
+    [method, req_target | _] = get_request_line(request)
+    resp = route_request(method, req_target)
     :gen_tcp.send(client, resp)
     :gen_tcp.close(client)
+  end
+
+  defp get_request_line(request) do
+    String.split(request, "\r\n")
+    |> List.first()
+    |> String.split(" ")
+  end
+
+  defp route_request(method, request_target) do
+    route_segments = String.split(request_target, "/", trim: true)
+
+    case {method, route_segments} do
+      {"GET", []} -> build_response(200, "")
+      {"GET", ["echo", to_echo]} -> build_response(200, to_echo)
+      {"GET", _} -> build_response(404, "")
+      {_, _} -> build_response(405, "")
+    end
+  end
+
+  defp build_response(status_code, body) do
+    status_msg =
+      case status_code do
+        200 -> "OK"
+        404 -> "Not Found"
+        405 -> "Method Not Allowed"
+      end
+
+    """
+    HTTP/1.1 #{status_code} #{status_msg}\r
+    Content-Type: text/plain\r
+    Content-Length: #{byte_size(body)}\r
+    \r
+    #{body}\
+    """
   end
 end
 
