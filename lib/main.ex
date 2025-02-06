1a240447ee675906e24e8b89457b8c38ad83ec0e
Christopher Quinn <caquinn7@gmail.com>
2025-02-05 19:58:00 -0600

    codecrafters submit [skip ci]

--- lib/main.ex
+++ lib/main.ex
@@ -1,3 +1,45 @@
+defmodule HttpRequest do
+  defstruct method: "", target: "", headers: %{}, body: ""
+
+  def parse(request) do
+    [before_body, body] = String.split(request, "\r\n\r\n", parts: 2)
+
+    before_body_lines = String.split(before_body, "\r\n")
+
+    request_line =
+      before_body_lines
+      |> List.first()
+      |> String.split(" ", parts: 3)
+
+    case request_line do
+      [method, target, _http_version] ->
+        headers =
+          before_body_lines
+          |> Enum.drop(1)
+          |> Enum.map(fn line ->
+            case String.split(line, ": ", parts: 2) do
+              # Well-formed header
+              [k, v] -> {k, v}
+              # Missing value → Default to empty string
+              [k] -> {k, ""}
+            end
+          end)
+          |> Enum.into(%{})
+
+        %HttpRequest{
+          method: method,
+          target: target,
+          headers: headers,
+          body: body
+        }
+
+      _ ->
+        # Return an empty request for malformed inputs
+        %HttpRequest{}
+    end
+  end
+end
+
 defmodule Server do
   use Application
 
@@ -14,60 +56,58 @@ defmodule Server do
     # ensures that we don't run into 'Address already in use' errors
     {:ok, socket} = :gen_tcp.listen(4221, [:binary, active: false, reuseaddr: true])
     IO.puts("Listening...")
-    {:ok, client} = :gen_tcp.accept(socket)
-    {:ok, request} = :gen_tcp.recv(client, 0)
-    IO.puts("Received request:\n#{request}")
-
-    {method, target, headers, _body} = destruct_request(request)
-    route_segments = String.split(target, "/", trim: true)
-
-    resp =
-      case {method, route_segments} do
-        {"GET", []} ->
-          build_response(200, "")
-
-        {"GET", ["echo", to_echo]} ->
-          build_response(200, to_echo)
-
-        {"GET", ["user-agent"]} ->
-          headers
-          |> Map.get("User-Agent", "")
-          |> then(fn x -> build_response(200, x) end)
-
-        {"GET", _} ->
-          build_response(404, "")
-
-        {_, _} ->
-          build_response(405, "")
-      end
-
-    :gen_tcp.send(client, resp)
-    :gen_tcp.close(client)
+    accept_loop(socket)
   end
 
-  defp destruct_request(request) do
-    [before_body, body] = String.split(request, "\r\n\r\n")
+  defp accept_loop(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    Task.start(fn -> handle_client(client) end)
+    accept_loop(socket)
+  end
 
-    before_body_lines = String.split(before_body, "\r\n")
+  defp handle_client(client) do
+    try do
+      case :gen_tcp.recv(client, 0, 5000) do
+        {:ok, request} ->
+          IO.puts("Received request:\n#{request}")
 
-    [method, target, _http_version] =
-      before_body_lines
-      |> List.first()
-      |> String.split(" ")
+          request = HttpRequest.parse(request)
+          response = route_request(request)
 
-    headers =
-      before_body_lines
-      |> Enum.drop(1)
-      |> Enum.map(fn line ->
-        line
-        |> String.split(": ", trim: true)
-        |> case do
-          [k, v] -> {k, v}
-        end
-      end)
-      |> Enum.into(%{})
+          :gen_tcp.send(client, response)
 
-    {method, target, headers, body}
+        {:error, :timeout} ->
+          IO.puts("Client timed out, closing connection.")
+      end
+    rescue
+      exception ->
+        IO.puts("Stacktrace: #{Exception.format(:error, exception, __STACKTRACE__)}")
+    after
+      :gen_tcp.close(client)
+    end
+  end
+
+  defp route_request(%HttpRequest{method: method, target: target, headers: headers}) do
+    route_segments = String.split(target, "/", trim: true)
+
+    case {method, route_segments} do
+      {"GET", []} ->
+        build_response(200, "")
+
+      {"GET", ["echo", to_echo]} ->
+        build_response(200, to_echo)
+
+      {"GET", ["user-agent"]} ->
+        headers
+        |> Map.get("User-Agent", "")
+        |> then(&build_response(200, &1))
+
+      {"GET", _} ->
+        build_response(404, "")
+
+      {_, _} ->
+        build_response(405, "")
+    end
   end
 
   defp build_response(status_code, body) do
